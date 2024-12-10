ad8f0e000561f850c10a4180973c4d926182034f
GuirraMv <matheusguirravieira15@gmail.com>
2024-12-09 22:05:43 -0300

    feat: adding message

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,36 @@
 function Home() {
-  return <h1>test</h1>;
+  return (
+    <div
+      style={{
+        flex: 1,
+        width: "100%",
+        height: "100%",
+        alignSelf: "center",
+        alignItems: "center",
+        justifyContent: "center",
+        display: "flex",
+        flexDirection: "column",
+        textAlign: "center",
+        fontFamily: "sans-serif",
+      }}
+    >
+      <h1
+        style={{
+          fontSize: 24,
+          padding: 0,
+          margin: 0,
+        }}
+      >
+        ✝️❤️
+        <br /> O meu mandamento é este: Que vos ameis uns aos outros, assim como
+        eu vos amei.
+        <br /> Ninguém tem maior amor do que este: de dar alguém a sua vida
+        pelos seus amigos.
+        <br /> Vós sereis meus amigos, se fizerdes o que eu vos mando.
+      </h1>
+      <h2 style={{ fontSize: 22 }}>João 15:12-14</h2>
+    </div>
+  );
 }
 
 export default Home;
